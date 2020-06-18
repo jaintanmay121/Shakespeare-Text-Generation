aaa5b909c0faac9252d820aa447d5c7882325393
Tanmay Jain <jaintanmay121@gmail.com>
2020-06-19 00:24:07 +0530

    tensorflow work

--- Text_Gen.py
+++ Text_Gen.py
@@ -9,7 +9,7 @@ char2index = {char: index for index, char in enumerate(vocab)}
 index2char = np.array(vocab)       
 
 
-def generate_text(model, start_string="Romeo", num_generate = 1000, temperature=1.0):
+def generate_text(model, start_string="Romeo:", num_generate = 1000, temperature=1.0):
     
     input_indices = [char2index[s] for s in start_string]
     input_indices = tf.expand_dims(input_indices, 0)
@@ -21,8 +21,10 @@ def generate_text(model, start_string="Romeo", num_generate = 1000, temperature=
         predictions = tf.squeeze(predictions, 0)
 
         predictions = predictions / temperature
-        predicted_id = tf.random.categorical(predictions,
-                                             num_samples=1)[-1,0]
+        predicted_id = str(tf.random.categorical(predictions,num_samples=1)[-1,0])
+        s=re.search('\(', predicted_id).start()+1
+        e=re.search(',', predicted_id).start()
+        predicted_id=int(predicted_id[s:e])
 
         input_indices = tf.expand_dims([predicted_id], 0)
 
